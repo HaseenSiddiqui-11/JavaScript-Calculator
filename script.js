9ef9e94fb3df512551908787efd867cd0b907aed
= <siddh9146@wrdsb.ca>
2022-03-21 10:04:09 -0400

    new commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const allCalculatorButtons = document.getElementsByTagName("button");
+const textField = document.getElementById("text-para")
+for (var i = 0; i < allCalculatorButtons.length; i++) {
+    allCalculatorButtons[i].addEventListener("click", calculatorButtonTriggered);
+}
+
+function calculatorButtonTriggered(button) {
+    button = button.target;
+    console.log(button);
+
+    if (button.innerHTML == "AC") {
+        textField.innerHTML = "aa";
+    }
+
+    else if (button.innerHTML == "C") {
+        textField.innerHTML = textField.innerHTML.split("").splice(0, textField.innerHTML.length - 1).join("");
+    }
+
+    else if (button.innerHTML == " = ") {
+        resolveQuestion();
+    }
+
+    else {
+        textField.innerHTML += button.innerHTML;
+    }
+}
+
+function resolveQuestion() {
+    textArray = textField.innerHTML.split('');
+    for (var i = 0; i < textArray.length; i++) {
+        if (textArray[i] == "x") {
+            textArray[i] = " * ";
+        }
+
+        if (textArray[i] == "÷") {
+            textArray[i] = " / ";
+        }
+    }
+
+
+    textField.innerHTML = eval(textArray.join(''));
+}
